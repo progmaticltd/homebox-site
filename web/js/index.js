beab908a7d229225d75b773d141e131766fed4d2
Andre Rodier <andre@lovelace.rodier.me>
2024-02-24 14:28:41 +0000

    Added dns features page

--- web/js/index.js
+++ web/js/index.js
@@ -6,11 +6,13 @@ let address = window.location.href;
 // List of known languages
 let languages = [ "en", "fr" ];
 
+const timer = 250;
+
 if ( ! languages.includes(lang) ) {
     console.log("Language not supported: " + lang);
 } else if ( location.pathname != "/" ) {
     let newAddress = address.replace(/index.html/, ("index-" + lang + ".html"));
-    window.setTimeout(() => { window.location.replace(newAddress) }, 3000);
+    window.setTimeout(() => { window.location.replace(newAddress) }, timer);
 } else {
-    window.setTimeout(() => { window.location.replace(address + "index-en.html") }, 3000);
+    window.setTimeout(() => { window.location.replace(address + "index-en.html") }, timer);
 }
